14c58c07e90104f2df2de99ed784831995ab98c3
Aneta <anc@praqma.net>
2024-04-21 11:31:50 +0200

    added tracking script

--- layouts/DefaultLayout/DefaultLayout.tsx
+++ layouts/DefaultLayout/DefaultLayout.tsx
@@ -75,6 +75,23 @@ export const DefaultLayout = ({
         {/* URL to an image for sharing */}
         <meta property="og:url" content="https://www.kultur-kortet.dk/" />
         <meta property="og:type" content="website" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            var _paq = window._paq = window._paq || [];
+            /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+            _paq.push(['trackPageView']);
+            _paq.push(['enableLinkTracking']);
+            (function() {
+              var u="https://nabohelsingorvercelapp.matomo.cloud/";
+              _paq.push(['setTrackerUrl', u+'matomo.php']);
+              _paq.push(['setSiteId', '1']);
+              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+              g.async=true; g.src='https://cdn.matomo.cloud/nabohelsingorvercelapp.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+            })();
+          `,
+          }}
+        />
       </Head>
 
       <Menu
